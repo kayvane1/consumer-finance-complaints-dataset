37b7d3b1d7bfdeb9198004749004aeca0d3f1ba4
kayvane <kayvane.shakerifar@gmail.com>
2022-01-23 16:39:41 +0000

    fixed dataset loading script

--- consumerComplaints.py
+++ consumerComplaints.py
@@ -1,4 +1,5 @@
 import datasets
+from datasets import TextClassification
 import csv
 
 logger = datasets.logging.get_logger(__name__)
@@ -6,24 +7,21 @@ logger = datasets.logging.get_logger(__name__)
 
 _DESCRIPTION = """\
 The Consumer Complaint Database is a collection of complaints about consumer financial products and services that we sent to companies for response. Complaints are published after the company responds, confirming a commercial relationship with the consumer, or after 15 days, whichever comes first. Complaints referred to other regulators, such as complaints about depository institutions with less than $10 billion in assets, are not published in the Consumer Complaint Database. The database generally updates daily.
-There are two features:
-  - article: text of news article, used as the document to be summarized
-  - highlights: joined text of highlights with <s> and </s> around each
-    highlight, which is the target summary
+There are multiple Text Classification problems that can be solved with this dataset:
+- Complaint Type Identification
+- Complaint Sub-Product Identification
+- Complaint from vulnerable or Service-person
 """
 
-# The second citation introduces the source data, while the first
-# introduces the specific form (non-anonymized) we use here.
-
 _CITATION = """\
-@
+@Consumer Financial Protection Bureau
 """
 
 _BASE_DOWNLOAD_URL = "https://files.consumerfinance.gov/ccdb/complaints.csv.zip"
 
-_HOMEPAGE = "todo"
+_HOMEPAGE = "https://www.consumerfinance.gov/data-research/consumer-complaints/"
 
-_LICENSE = "todo"
+_LICENSE = "https://cfpb.github.io/source-code-policy/"
 
 _SUPPORTED_VERSIONS = [
     datasets.Version("1.0.0", "Initial Version"),
@@ -32,76 +30,15 @@ _SUPPORTED_VERSIONS = [
 
 _DEFAULT_VERSION = datasets.Version("1.0.0", "Initial Version")
 
-
-
 _url = _BASE_DOWNLOAD_URL
 
-
-class consumerComplaints(datasets.GeneratorBasedBuilder):
-    """Imagewang dataset."""
-
-    #BUILDER_CONFIGS = [ImagewangConfig(name=name) for name in _BASE_CONFIG_NAMES]
-
-    #DEFAULT_CONFIG_NAME = "full-size"
-
-    def _info(self):
-        features = datasets.Features(
-            {
-                'date_received' : datasets.Value("timestamp[s]"),
-                'product': datasets.ClassLabel(names=_PRODUCT_CATEGORIES), 
-                'sub_product': datasets.ClassLabel(names=_SUB_PRODUCT_CATEGORIES), 
-                'issue': datasets.Value("string"), 
-                'sub_issue': datasets.Value("string"),
-                'complaint_text': datasets.Value("string"), 
-                'company_public_response': datasets.Value("string"), 
-                'company': datasets.Value("string"),
-                'state': datasets.Value("string"), 
-                'zip_code': datasets.Value("string"), 
-                'tags': datasets.Value("string"), 
-                'consumer_consent_provided' : datasets.Value("string"),
-                'submitted_via': datasets.Value("string"), 
-                'date_sent_to_company': datasets.Value("string"), 
-                'company_response_to_consumer': datasets.Value("string"),
-                'timely_response': datasets.Value("string"), 
-                'consumer_disputed': datasets.Value("string"), 
-                'complaint_id': datasets.Value("string")
-            }
-        )
-        return datasets.DatasetInfo(
-            description=_DESCRIPTION,
-            features=features,
-            homepage=_HOMEPAGE,
-            license=_LICENSE,
-            citation=_CITATION,
-            #task_templates=[ImageClassification(image_column="image", label_column="label")],
-        )
-
-    def _split_generators(self, dl_manager):
-        path = dl_manager.download_and_extract(_url)
-
-        return [
-            datasets.SplitGenerator(
-                name=datasets.Split.TRAIN,
-                gen_kwargs={"filepath": path},
-            )
-        ]
-
-    def _generate_examples(self, filepath):
-        """Generate consumer complaints examples."""
-        with open(filepath, encoding="utf-8") as csv_file:
-            csv_reader = csv.reader(
-                csv_file, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True
-            )
-            for id_, row in enumerate(csv_reader):
-                date_received, product, sub_product, issue, sub_issue, complaint_text, company_public_response, company, state, zip_code, tags, consumer_consent_provided, submitted_via, date_sent_to_company, company_response_to_consumer, timely_response, consumer_disputed, complaint_id    = row
-                yield id_, {"Date Recieved": date_received, "Product": product, "Sub Product": sub_product, "Issue": issue, "Sub Issue":sub_issue, "Complaint Text" : complaint_text, "Company Public Response" :company_public_response, "Company" : company, "State": state, "Zip Code" :zip_code, "tags" : tags, "Consumer Consent Provided" : consumer_consent_provided, "Submitted via": submitted_via, "Date sent to company" :  date_sent_to_company, "Company Response To Consumer" : company_response_to_consumer, "Timely response" : timely_response, "Consumer disputed":consumer_disputed , "Complaint ID": complaint_id }
-
-_TAGS = ['Servicemember', 'Older American', 'Older American, Servicemember']
+_TAGS = ['Servicemember', 'Older American', 'Older American, Servicemember', '']
 
 _SUB_PRODUCT_CATEGORIES = ['Credit reporting',
  'General-purpose credit card or charge card',
  'Checking account',
  'Other debt',
+ 'Second mortgage',
  'Conventional home mortgage',
  'I do not know',
  'Credit card debt',
@@ -172,10 +109,12 @@ _SUB_PRODUCT_CATEGORIES = ['Credit reporting',
  'Traveler’s/Cashier’s checks',
  'Transit card',
  'Student prepaid card',
- 'Electronic Benefit Transfer / EBT card']
+ 'Electronic Benefit Transfer / EBT card',
+ '']
 
-_PRODUCT_CATEGORIES =['Credit reporting, credit repair services, or other personal consumer reports',
- 'Debt collection',
+_PRODUCT_CATEGORIES =[
+ 'Credit reporting, credit repair services, or other personal consumer reports', 
+ 'Debt collection', 
  'Mortgage',
  'Credit card or prepaid card',
  'Checking or savings account',
@@ -191,4 +130,66 @@ _PRODUCT_CATEGORIES =['Credit reporting, credit repair services, or other person
  'Money transfers',
  'Prepaid card',
  'Other financial service',
- 'Virtual currency']
+ 'Virtual currency',
+ ]
+
+class consumerComplaints(datasets.GeneratorBasedBuilder):
+    """ Consumer Complaints dataset."""
+
+    def _info(self):
+        features = datasets.Features(
+            {
+                'Date Received' : datasets.Value("timestamp[s]"),
+                'Product': datasets.features.ClassLabel(names=_PRODUCT_CATEGORIES), 
+                'Sub Product': datasets.features.ClassLabel(names=_SUB_PRODUCT_CATEGORIES), 
+                'Issue': datasets.Value("string"), 
+                'Sub Issue': datasets.Value("string"),
+                'Complaint Text': datasets.Value("string"), 
+                'Company Public Response': datasets.Value("string"), 
+                'Company': datasets.Value("string"),
+                'State': datasets.Value("string"), 
+                'Zip Code': datasets.Value("string"), 
+                'Tags': datasets.features.ClassLabel(names=_TAGS), 
+                'Consumer Consent Provided' : datasets.Value("string"),
+                'Submitted via': datasets.Value("string"), 
+                'Date Sent To Company': datasets.Value("string"), 
+                'Company Response To Consumer': datasets.Value("string"),
+                'Timely Response': datasets.Value("string"), 
+                'Consumer Disputed': datasets.Value("string"), 
+                'Complaint ID': datasets.Value("string")
+            }
+        )
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=features,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+            task_templates=[
+              TextClassification(text_column="Complaint Text", label_column="Product"),
+              TextClassification(text_column="Complaint Text", label_column="Sub Product"),
+              TextClassification(text_column="Complaint Text", label_column="Tags"),
+            ],
+        )
+
+    def _split_generators(self, dl_manager):
+        path = dl_manager.download_and_extract(_url)
+        path = f"{path}/complaints.csv"
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"filepath": path},
+            )
+        ]
+
+    def _generate_examples(self, filepath):
+        """Generate consumer complaints examples."""
+        with open(filepath, encoding="utf-8") as csv_file:
+            csv_reader = csv.reader(
+                csv_file, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True
+            )
+            next(csv_reader) #skip header row
+            for id_, row in enumerate(csv_reader):
+                date_received, product, sub_product, issue, sub_issue, complaint_text, company_public_response, company, state, zip_code, tags, consumer_consent_provided, submitted_via, date_sent_to_company, company_response_to_consumer, timely_response, consumer_disputed, complaint_id    = row
+                yield id_, {"Date Received": date_received, "Product": product, "Sub Product": sub_product, "Issue": issue, "Sub Issue":sub_issue, "Complaint Text" : complaint_text, "Company Public Response" :company_public_response, "Company" : company, "State": state, "Zip Code" :zip_code, "Tags" : tags, "Consumer Consent Provided" : consumer_consent_provided, "Submitted via": submitted_via, "Date Sent To Company" :  date_sent_to_company, "Company Response To Consumer" : company_response_to_consumer, "Timely Response" : timely_response, "Consumer Disputed":consumer_disputed , "Complaint ID": complaint_id }
+
